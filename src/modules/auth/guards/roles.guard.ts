21c7235de60a40e02faf1c10b224d5bb4f9c8d4c
Muhammad Amir <expressdev093@gmail.com>
2022-08-27 13:03:55 +0500

    final commit

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -22,8 +22,6 @@ export class RolesGuard implements CanActivate {
       user = request.user;
     }
 
-    console.log(roles, user);
-
     return roles.some((role) => user.role === role);
   }
 }
--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -1,16 +1,12 @@
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { config } from 'dotenv';
 config({
-  path: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+  path: `${process.cwd()}/src/config/env/${process.env.NODE_ENV}.env`,
 });
-
-const configService = new ConfigService();
-
 export default new DataSource({
   type: 'sqlite',
-  database: 'devdb',
+  database: process.env.DB_NAME,
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   migrations: ['dist/src/database/migrations/*{.ts,.js}'],
   synchronize: false,
